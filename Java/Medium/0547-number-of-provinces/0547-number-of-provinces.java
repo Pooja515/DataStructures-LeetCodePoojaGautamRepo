980352598f116ddc66609ab0193ce7509a5ce5d6
POOJA GAUTAM <58748949+Pooja515@users.noreply.github.com>
2026-07-07 21:22:36 +0530

    Time: 1 ms (87.18%), Space: 47 MB (87.71%) - LeetHub

--- Java/Medium/0547-number-of-provinces/0547-number-of-provinces.java
+++ Java/Medium/0547-number-of-provinces/0547-number-of-provinces.java
@@ -1,29 +1,23 @@
 class Solution {
     public int findCircleNum(int[][] isConnected) {
-
         int n = isConnected.length;
         boolean[] visited = new boolean[n];
-        int province =0;
-
-        for(int i = 0;i < n ;i++){
-           
-            if(!visited[i]){
-                dfs(i,isConnected,visited,n );
+        int province = 0;
+        for(int i = 0;i<n;i++){
+             if(!visited[i]){
+                dfs(i,isConnected,visited,n);
                 province++;
-              }
-           
+             }
         }
         return province;
     }
 
-    void dfs(int i , int[][] isConnected , boolean[] visited ,int n){
-        visited[i]= true;
-        
-            for(int j = 0; j<n;j++){
-                if(!visited[j] && isConnected[i][j] == 1){
-                    dfs(j,isConnected, visited,n);
-                }
+    void dfs(int i , int[][] isConnected , boolean[] visited , int n){
+        visited[i] = true;
+        for(int j = 0 ;j<n ;j++){
+            if(!visited[j] && isConnected[i][j] == 1){
+                dfs(j,isConnected,visited,n);
             }
-        
+        }
     }
 }
