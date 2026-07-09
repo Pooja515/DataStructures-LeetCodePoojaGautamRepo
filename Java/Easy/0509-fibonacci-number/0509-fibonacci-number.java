845a99ca09ef66bdbd3322c048f21671028f002c
POOJA GAUTAM <58748949+Pooja515@users.noreply.github.com>
2026-07-09 19:25:29 +0530

    Time: 0 ms (100%), Space: 42 MB (69.56%) - LeetHub

--- Java/Easy/0509-fibonacci-number/0509-fibonacci-number.java
+++ Java/Easy/0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,7 +1,17 @@
 class Solution {
+    int[] memo;
     public int fib(int n) {
         if(n<=1) return n;
+        memo = new int[n+1];
+        Arrays.fill(memo , -1);
 
-        return fib(n-1) + fib(n-2);
+        return f(n);
+    }
+    int f(int n){
+        if(n <= 1) return n;
+
+        if(memo[n] != -1) return memo[n];
+
+        return memo[n] = f(n-1)+f(n-2);
     }
 }
