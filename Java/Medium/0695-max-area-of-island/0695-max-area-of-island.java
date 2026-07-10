8137e0b1d4eb4eb85ab246a053c8926f9e026735
POOJA GAUTAM <58748949+Pooja515@users.noreply.github.com>
2026-07-10 16:15:57 +0530

    Time: 4 ms (10.62%), Space: 46.8 MB (15.76%) - LeetHub

--- Java/Medium/0695-max-area-of-island/0695-max-area-of-island.java
+++ Java/Medium/0695-max-area-of-island/0695-max-area-of-island.java
@@ -1,27 +1,40 @@
 class Solution {
-    int[][] dir = {{-1,0},{1,0} , {0,-1},{0,1}};
+    int[][] dirs = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
     public int maxAreaOfIsland(int[][] grid) {
-        int maxArea = 0;
+        if(grid == null || grid[0].length == 0) return 0;
         int m = grid.length , n = grid[0].length;
-        if(grid == null || m==0) return 0;
         boolean[][] visited = new boolean[m][n];
-        for(int r=0;r<m;r++){
+         int maxArea = 0;
+        for(int r = 0 ;r<m;r++){
             for(int c=0;c<n;c++){
-                 if(grid[r][c] == 1 && !visited[r][c]){
-                    maxArea = Math.max(maxArea , dfs(r,c,grid,visited,m,n));
-                 } 
+                if(grid[r][c] == 1 && !visited[r][c] ){
+                    maxArea = Math.max(maxArea , bfs(r,c,grid,visited));
+                }
             }
         }
         return maxArea;
     }
-    int dfs(int r,int c,int[][] grid , boolean[][] visited ,int m ,int n){
-        int area =1;
+    int bfs(int r , int c , int[][] grid , boolean[][] visited){
+        int m = grid.length , n = grid[0].length;
+        int area = 1;
+
+        Queue<int[]> q = new ArrayDeque<>();
+        q.offer(new int[] {r,c});
         visited[r][c] = true;
-        for(int[] d : dir){
-            int newr = r + d[0] , newc = c + d[1];
-            if(newr >= 0 && newr <m && newc >= 0 && newc <n && grid[newr][newc] == 1 && !visited[newr][newc]){
-                area += dfs(newr , newc , grid, visited , m , n);
+
+        while(!q.isEmpty()){
+            int[] cur = q.poll();
+            for(int[] d : dirs){
+                int newr =cur[0] + d[0] ,newc = cur[1] + d[1];
+        
+                if(newr >= 0 && newr < m && newc >= 0 && newc < n && grid[newr][newc] == 1 && !visited[newr][newc] ){
+                 visited[newr][newc] = true;
+                 q.offer(new int[] {newr , newc});
+                 area++;
+                
+                }
             }
+           
         }
         return area;
     }
