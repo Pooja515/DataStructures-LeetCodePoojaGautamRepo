c7c64797cb60045aa849c9224d4e50ab96ab65c7
POOJA GAUTAM <58748949+Pooja515@users.noreply.github.com>
2026-07-12 21:57:03 +0530

    Time: 42 ms (84.33%), Space: 177.1 MB (90.92%) - LeetHub

--- Java/Medium/1765-map-of-highest-peak/1765-map-of-highest-peak.java
+++ Java/Medium/1765-map-of-highest-peak/1765-map-of-highest-peak.java
@@ -1,35 +1,33 @@
 class Solution {
     public int[][] highestPeak(int[][] isWater) {
-        if (isWater == null || isWater.length == 0)
-            return isWater;
-        Queue<int[]> q = new LinkedList<>();
-        int rows = isWater.length, cols = isWater[0].length;
-        int distance = 0;
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
-                if (isWater[i][j] == 1) {
-                    q.offer(new int[] { i, j });
-                    isWater[i][j] = 0; // Important
-                } else {
-                    isWater[i][j] = -1;
-                }
-            }
-        }
-        int[][] directions = { { -1, 0 }, { 1, 0 }, { 0, -1 }, { 0, 1 } };
-        while (!q.isEmpty()) {
-            int[] cell = q.poll();
-            int r = cell[0];
-            int c = cell[1];
-            for (int[] d : directions) {
-                int newr = r + d[0];
-                int newc = c + d[1];
-                if (newr >= 0 && newr < rows && newc >= 0 && newc < cols && isWater[newr][newc] == -1) {
-                    isWater[newr][newc] = isWater[r][c] + 1;
-                    q.offer(new int[] { newr, newc });
-                }
-            }
-        }
+        int m = isWater.length, n = isWater[0].length;
 
+        Queue<int[]> q = new LinkedList<>();
+
+        for (int r = 0; r < m; r++) {
+            for (int c = 0; c < n; c++) {
+                if (isWater[r][c] == 1) {
+                    q.offer(new int[] { r, c });
+                    isWater[r][c] = 0;
+                }
+                else{
+                    isWater[r][c] = -1;
+                }
+            }
+        }
+        int[][] dir = {{-1,0},{1,0},{0,1},{0,-1}};
+        while(!q.isEmpty()){
+            int[] cur = q.poll();
+            for(int[] d : dir){
+                int newr = cur[0] + d[0];
+                int newc = cur[1] + d[1];
+
+                if(newr >= 0 && newr < m && newc >= 0 && newc < n &&  isWater[newr][newc] == -1){
+                    isWater[newr][newc] = isWater[cur[0]][cur[1]] + 1;
+                    q.offer( new int[] { newr , newc});
+                }
+            }
+        }
         return isWater;
     }
 }
