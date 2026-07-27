2e18497b445fb6070a12f8b3915178b227af9145
POOJA GAUTAM <58748949+Pooja515@users.noreply.github.com>
2026-07-27 13:30:45 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ Java/Hard/0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -0,0 +1,34 @@
+class Solution {
+    public String minWindow(String s, String t) {
+        int n = t.length(), m = s.length();
+
+        if(m < n) return "";
+
+        int[] freq = new int[128];
+
+        for(int i =0 ; i<n ;i++){
+            char c = t.charAt(i);
+            freq[c]++;
+        }
+        int cnt = n , l=0 , minlen = Integer.MAX_VALUE , start =0;
+        for(int r = 0 ;r<m;r++){
+            char c = s.charAt(r);
+            if(freq[c] > 0){
+                cnt--;
+            }
+            freq[c]--;
+            while(cnt == 0){
+                if(r-l+1 < minlen){
+                    minlen = r-l+1;
+                    start = l;
+                }
+                char left = s.charAt(l);
+                freq[left]++;
+                if(freq[left] >0) cnt++;
+                l++;
+            }
+        }
+        return minlen == Integer.MIN_VALUE ? "" : s.substring(start,start + minlen);
+    }
+}
+                
