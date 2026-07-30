70d4e2130ccfccdb8c4ba07383f8f814b4f8d929
POOJA GAUTAM <58748949+Pooja515@users.noreply.github.com>
2026-07-30 11:18:43 +0530

    Time: 40 ms (74.14%), Space: 47.7 MB (86.21%) - LeetHub

--- Java/Medium/1631-path-with-minimum-effort/1631-path-with-minimum-effort.java
+++ Java/Medium/1631-path-with-minimum-effort/1631-path-with-minimum-effort.java
@@ -1,72 +1,44 @@
 class Solution {
-
     public int minimumEffortPath(int[][] heights) {
+        int n = heights.length;
+        int m = heights[0].length;
 
-        int rows = heights.length;
-        int cols = heights[0].length;
-        // 1. Store minimum effort required to reach each cell
-        int[][] minEffort =  new int[rows][cols];
-
-        for (int[] row : minEffort) {
-            Arrays.fill(row,Integer.MAX_VALUE );
+        //PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) ->Integer.compare(a[0],b[0]));
+        PriorityQueue<int[]> pq =new PriorityQueue<>((a, b) ->Integer.compare(a[0],b[0]));
+        int[][] distance = new int[n][m];
+        for(int[] row : distance){
+                Arrays.fill(row,(int) 1e9);
         }
-        // 2. PriorityQueue
-        // {row, col, effort}
-     
-        PriorityQueue<int[]> pq =new PriorityQueue<>((a, b) ->Integer.compare(a[2],b[2]));
-        // 3. Start from (0, 0)
-        minEffort[0][0] = 0;
 
-        pq.offer(new int[]{ 0,0,0});
-        // Directions:
-        // up, down, left, right
+       distance[0][0] = 0; // source (0,0) =0;
 
-        int[][] directions = {{-1, 0},{1, 0},{0, -1}, {0, 1} };
+       pq.offer(new int[] {0,0,0}); // pair {distance , r,c};
 
-        // 4. Dijkstra
-       
-        while (!pq.isEmpty()) {
+       int[][] dir = {{-1,0} ,{1,0} ,{0,-1} ,{0,1}}; // 4 directions
 
-            int[] current = pq.poll();
+       while(!pq.isEmpty()){
+        int[] curr = pq.poll();
+        int currdistance = curr[0];
+        int r = curr[1];
+        int c = curr[2];
 
-            int row = current[0];
+        if(r == n-1 && c == m-1){
+            return currdistance;
+        }
 
-            int col =current[1];
+        for(int[] d :dir){
+            int newr = r+d[0] , newc = c+d[1];
+            if(newr >= 0 && newr < n && newc >= 0 && newc < m ){
+                int newdistance = Math.max(Math.abs(heights[r][c] - heights[newr][newc]),currdistance );
 
-            int currentEffort = current[2];
-
-            // We reached destination with minimum effort
-            if (row == rows - 1 && col == cols - 1) {
-                return currentEffort;
-            } 
-            // 5. Explore four neighbors
-           
-            for (int[] direction :directions) {
-
-                int nextRow =row + direction[0];
-                int nextCol =col + direction[1];
-                // Check boundarie
-                if (nextRow < 0 || nextRow >= rows || nextCol < 0 || nextCol >= cols) {
-                    continue;
-                }
-                // Calculate effort of this single move
-
-                int edgeEffort =Math.abs(heights[row][col] - heights[nextRow][nextCol]);
-                // Path effort is the maximum effort
-                // encountered on the entire path
-
-                int newEffort = Math.max(currentEffort,edgeEffort );
-                // Relaxation
-                if (newEffort < minEffort[nextRow][nextCol]) {
-                         minEffort[nextRow][nextCol] =newEffort;
-
-
-                    pq.offer(new int[]{ nextRow,nextCol,newEffort } );
+                if(newdistance < distance[newr][newc]){
+                    distance[newr][newc] = newdistance;
+                    pq.offer(new int[]{newdistance , newr , newc});
                 }
             }
         }
 
-
-        return 0;
+       }
+       return 0;
     }
 }
