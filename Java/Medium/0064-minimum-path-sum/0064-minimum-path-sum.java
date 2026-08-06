77af345a8386d65b57c8149073cd8d8874ffafd2
POOJA GAUTAM <58748949+Pooja515@users.noreply.github.com>
2026-08-06 21:28:02 +0530

    Time: 1 ms (99.9%), Space: 50 MB (86.01%) - LeetHub

--- Java/Medium/0064-minimum-path-sum/0064-minimum-path-sum.java
+++ Java/Medium/0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -1,27 +1,31 @@
 class Solution {
-
+    int[][] memo;
     public int minPathSum(int[][] grid) {
-        int m = grid.length, n = grid[0].length;
+        int m = grid.length , n = grid[0].length;
+        if(m==0 || n==0) return 0;
+        memo = new int[m][n];
+        for(int[] rows : memo){
+            Arrays.fill(rows,-1);
+        }
 
-        int[] cur = new int[n];
+        return f(m-1,n-1,grid) == (int) 1e9 ? -1:f(m-1,n-1,grid);
+    }
+    int f(int i, int j , int[][] grid){
+        //base case
+        if(i==0 && j==0) return grid[0][0];
+        if(i<0 || j<0) return (int) 1e9;
+        int left= (int) 1e9;
+        int up = (int) 1e9;
 
-        for(int i=0 ;i<m ;i++){
-            for(int j =0;j<n ;j++){
-                 if (i == 0 && j == 0){
-                     cur[j] = grid[i][j];
-                     continue;
-                 }
+        if(memo[i][j] != -1) return memo[i][j];
+        if(i>0) {
+            up = f(i-1,j,grid);
+        }
+        if(j>0){
+            left = f(i,j-1,grid);
+        }
 
-            int up = (int) 1e9;
-            int left = (int) 1e9;
-    
-            if (i > 0) up = cur[j];
-            if (j > 0) left = cur[j - 1];
-
-            cur[j] = grid[i][j] + Math.min(up, left);
-         }
-             
-     }
-    return cur[n-1];
+       memo[i][j] = grid[i][j] + Math.min(up , left);
+       return memo[i][j];
     }
 }
