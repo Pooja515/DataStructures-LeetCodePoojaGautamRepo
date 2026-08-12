10002d207589e4a69e1d0399f30148140bfe4705
POOJA GAUTAM <58748949+Pooja515@users.noreply.github.com>
2026-08-13 02:53:37 +0530

    Time: 21 ms (33.35%), Space: 70.7 MB (40.62%) - LeetHub

--- Java/Medium/0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.java
+++ Java/Medium/0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.java
@@ -1,30 +1,15 @@
 class Solution {
-    int[][] memo;
+   
     public int maxProfit(int[] prices, int fee) {
         int n = prices.length;
-        memo = new int[n][2];
-        for(int[] rows : memo){
-            Arrays.fill(rows,-1);
-        }
-
-        return f(0,1,fee,prices);
-    }
-
-    int f(int i , int buy , int fee,int[] prices){
-        if( i == prices.length) return 0;
-
-        if(memo[i][buy] != -1) return memo[i][buy];
-
-        if(buy == 1){
-            int buytoday = -prices[i] + f(i+1,0,fee,prices);
-            int skipbuy = 0 + f(i+1,1,fee,prices);
-
-            return memo[i][buy] = Math.max(buytoday, skipbuy);
-        }
-        else{
-            int selltoday = prices[i] + f(i+1,1,fee,prices) - fee;
-            int skipsell = 0 + f(i+1,0,fee,prices);
-            return memo[i][buy]=Math.max(selltoday,skipsell);
-        }
+         int[][] dp = new int[n+1][2];
+         dp[n][0] =0;
+         dp[n][1] =0;
+       
+         for(int i=n-1;i>=0;i--){
+            dp[i][0]= Math.max(prices[i] + dp[i+1][1] - fee ,dp[i+1][0]);
+            dp[i][1]= Math.max(-prices[i] + dp[i+1][0] ,dp[i+1][1]);
+         }
+        return dp[0][1];
     }
 }
