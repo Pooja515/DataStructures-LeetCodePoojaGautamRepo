d875796ae87fe33fdc4ee5abd7e9be2b57db6b8c
POOJA GAUTAM <58748949+Pooja515@users.noreply.github.com>
2026-08-13 14:40:41 +0530

    Time: 376 ms (12.88%), Space: 237.5 MB (5.32%) - LeetHub

--- Java/Medium/0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ Java/Medium/0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,34 +1,30 @@
 class Solution {
     int[][] memo;
     public int lengthOfLIS(int[] nums) {
-        int n = nums.length;
-        memo = new int[n][n+1];
-        for(int[] rows : memo){
-            Arrays.fill(rows,-1);
-        }
+      int n= nums.length;
+     
+      memo = new int[n][n+1];
+      for(int[] rows:memo){
+          Arrays.fill(rows,-1);
+      }
 
-        return f(0,-1,nums);
+      return f(0,-1,nums);  
+
     }
-   
-    
-    int f(int ind ,int prev_ind ,int[] nums){
+
+    int f(int ind , int prev_ind,int[] nums){
 
         if(ind == nums.length) return 0;
 
-        if(memo[ind][prev_ind+1] != -1) 
-                return memo[ind][prev_ind+1];
+        if(memo[ind][prev_ind+1] != -1) return memo[ind][prev_ind+1];
 
-        // not take 
-    
-        int nottake= 0 + f(ind+1, prev_ind,nums);
-
-        //take 
-        int take=0;
+        //nottake
+        int nottake = 0 + f(ind+1,prev_ind,nums);
+        int take =0;
         if(prev_ind == -1 || nums[ind] > nums[prev_ind]){
-            take = 1 + f(ind+1,ind,nums);
-            
+             take =1 + f(ind+1,ind,nums);
         }
-
-       return memo[ind][prev_ind+1] = Math.max(take,nottake);
+        int len = Math.max(take,nottake);
+        return memo[ind][prev_ind+1]= len;
     }
 }
