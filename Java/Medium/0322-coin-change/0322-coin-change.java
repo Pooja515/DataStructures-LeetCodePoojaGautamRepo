dcc1fcacf11ac1daa1e2f64e04175733274aaa1f
POOJA GAUTAM <58748949+Pooja515@users.noreply.github.com>
2026-08-15 18:19:29 +0530

    Time: 14 ms (91.05%), Space: 47.2 MB (16.76%) - LeetHub

--- Java/Medium/0322-coin-change/0322-coin-change.java
+++ Java/Medium/0322-coin-change/0322-coin-change.java
@@ -1,40 +1,29 @@
 class Solution {
-    int[][] memo;
+
     public int coinChange(int[] coins, int amount) {
-        int n= coins.length;
-        memo= new int[n][amount+1];
+        int n = coins.length;
+        int[][] dp = new int[n][amount + 1];
 
-        for(int[] rows:memo){
-            Arrays.fill(rows,-1);
+        for (int t = 0; t <= amount; t++) {
+            if (t % coins[0] == 0) {
+                dp[0][t] = t / coins[0];
+            } else
+                dp[0][t] = (int) 1e9;
+
         }
 
-        if(amount == 0) return 0;
-        if(n==1){
-            if(amount % coins[0] == 0){
-                return amount/coins[0];
+        for (int ind = 1; ind < n; ind++) {
+            for (int t = 0; t <= amount; t++) {
+                //nottake
+                int nottake = 0 + dp[ind - 1][t];
+                int take = (int) 1e9;
+                if (coins[ind] <= t) {
+                    take = 1 + dp[ind][t - coins[ind]];
+                }
+
+                dp[ind][t] = Math.min(take, nottake);
             }
-            else
-                 return -1;
         }
-        
-        return f(0,amount ,coins) >= (int) 1e9 ?-1:f(0,amount ,coins);
-    }
-    int f(int ind ,int amount , int[] coins){
-
-        //base case 
-        if(amount == 0) return 0;
-
-        if(ind == coins.length) return (int) 1e9;
-
-        if(memo[ind][amount] != -1) return memo[ind][amount];
-
-        //nottake
-        int nottake = 0 + f(ind+1,amount,coins);
-        int take = (int) 1e9;
-        if(coins[ind] <= amount){
-            take = 1 + f(ind,amount - coins[ind] , coins);
-        }
-
-        return memo[ind][amount] = Math.min(take,nottake);
+        return dp[n-1][amount] >= (int) 1e9 ?-1:dp[n-1][amount];
     }
 }
