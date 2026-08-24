acb4a5e19e06d47989e938d81f8a5f9cacd3a910
POOJA GAUTAM <58748949+Pooja515@users.noreply.github.com>
2026-08-25 03:37:16 +0530

    Time: 1 ms (17.34%), Space: 43.6 MB (46.01%) - LeetHub

--- Java/Medium/0063-unique-paths-ii/0063-unique-paths-ii.java
+++ Java/Medium/0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -1,31 +1,35 @@
 class Solution {
-    int[][] memo;
+
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
         if(obstacleGrid == null || obstacleGrid.length == 0 ) return 0;
         int m= obstacleGrid.length , n= obstacleGrid[0].length;
 
-        memo= new int[m][n];
-        for(int[] rows : memo){
-            Arrays.fill(rows,-1);
-        }
-
         if(obstacleGrid[0][0] == 1 || obstacleGrid[m-1][n-1] == 1) return 0;
 
-        return f(m-1,n-1,obstacleGrid);
+        int[][] dp = new int[m][n];
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(i==0 && j==0){
+                    dp[i][j] =1;
+                }
 
-    }
-    int f(int i , int j, int[][]  obstacleGrid){
-        // base case
-        if(i==0 && j==0 ) return 1;
-        if(i<0 || j<0 || i > obstacleGrid.length-1 || j>obstacleGrid[0].length-1) return 0;
-        if(obstacleGrid[i][j] == 1) return 0;
+                else{
 
-        if(memo[i][j] != -1) return memo[i][j];
-        
-        int up =0,left =0;
-        if(i > 0) up = f(i-1,j,obstacleGrid);
-        if(j > 0) left = f(i,j-1,obstacleGrid);
+                    if(i<0 || j<0) dp[i][j] = 0;
+                    if(obstacleGrid[i][j] == 1) {
+                        dp[i][j] = 0;
+                        continue;
+                    }
 
-        return memo[i][j] = up+left;
+                    int up =0,left =0;
+                    if(i > 0) up = dp[i-1][j];
+                    if(j > 0) left = dp[i][j-1];
+
+                    dp[i][j] = up+left;
+                            }
+                        }
+                    }
+       return dp[m-1][n-1]; 
+    
     }
 }
