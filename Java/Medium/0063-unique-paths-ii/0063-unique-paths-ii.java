17109beec031327b8ee179ea5d56559593a26113
POOJA GAUTAM <58748949+Pooja515@users.noreply.github.com>
2026-08-25 03:04:30 +0530

    Time: 0 ms (100%), Space: 43.4 MB (65.03%) - LeetHub

--- Java/Medium/0063-unique-paths-ii/0063-unique-paths-ii.java
+++ Java/Medium/0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -1,36 +1,31 @@
 class Solution {
-
+    int[][] memo;
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
-        if (obstacleGrid == null || obstacleGrid.length == 0)
-            return 0;
+        if(obstacleGrid == null || obstacleGrid.length == 0 ) return 0;
+        int m= obstacleGrid.length , n= obstacleGrid[0].length;
 
-        int m = obstacleGrid.length, n = obstacleGrid[0].length;
-
-        if (obstacleGrid[0][0] == 1 || obstacleGrid[m - 1][n - 1] == 1)
-            return 0;
-
-       int[] cur = new int[n];
-
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (i == 0 && j == 0) {
-                      cur[j] =1;
-                      continue;
-                }
-                
-                if (obstacleGrid[i][j] == 1){
-                    cur[j] =  0;
-                    continue;
-                }
-                
-
-                int up = 0 , left = 0;
-                if(i>0) up =   cur[j];
-                if(j>0) left = cur[j - 1];
-
-                cur[j] = up + left;
-            }
+        memo= new int[m][n];
+        for(int[] rows : memo){
+            Arrays.fill(rows,-1);
         }
-       return cur[n-1];
+
+        if(obstacleGrid[0][0] == 1 || obstacleGrid[m-1][n-1] == 1) return 0;
+
+        return f(m-1,n-1,obstacleGrid);
+
+    }
+    int f(int i , int j, int[][]  obstacleGrid){
+        // base case
+        if(i==0 && j==0 ) return 1;
+        if(i<0 || j<0 || i > obstacleGrid.length-1 || j>obstacleGrid[0].length-1) return 0;
+        if(obstacleGrid[i][j] == 1) return 0;
+
+        if(memo[i][j] != -1) return memo[i][j];
+        
+        int up =0,left =0;
+        if(i > 0) up = f(i-1,j,obstacleGrid);
+        if(j > 0) left = f(i,j-1,obstacleGrid);
+
+        return memo[i][j] = up+left;
     }
 }
