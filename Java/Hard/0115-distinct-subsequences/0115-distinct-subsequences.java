fbdc819dbf2047316c0c3e77b4812ec8a01056ac
POOJA GAUTAM <58748949+Pooja515@users.noreply.github.com>
2026-09-03 03:51:42 +0530

    Time: 20 ms (53.43%), Space: 54.6 MB (21.02%) - LeetHub

--- Java/Hard/0115-distinct-subsequences/0115-distinct-subsequences.java
+++ Java/Hard/0115-distinct-subsequences/0115-distinct-subsequences.java
@@ -1,26 +1,33 @@
 class Solution {
 
     public int numDistinct(String s, String t) {
-        int m = s.length() , n= t.length();
-        if(m <n) return 0;
+        int m = s.length(), n = t.length();
+        if (n == 0)
+            return 0;
+        if (m < n)
+            return 0;
 
-        int[][] dp= new int[m+1][n+1];
+        int[][] dp = new int[m + 1][n + 1];
 
-        for(int ind1 =0 ;ind1 <= m;ind1++){
-            dp[ind1][0] = 1;
+        for(int i=0;i<=m;i++){
+            dp[i][0] =1;
         }
 
-        for(int ind1=1;ind1 <= m; ind1++){
-            for(int ind2 = 1 ; ind2 <= n ; ind2++){
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (j == 0)
+                    dp[0][0] = 1;
+                if (i == 0)
+                    dp[i][j] = 0;
+                // take
+                else if (s.charAt(i-1) == t.charAt(j-1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
+                } else {
+                    dp[i][j] = dp[i - 1][j];
+                }
 
-               if(s.charAt(ind1-1) == t.charAt(ind2-1)){
-                    dp[ind1][ind2]= dp[ind1-1][ind2-1] + dp[ind1-1][ind2];
-               }
-                else{
-                      dp[ind1][ind2]=dp[ind1-1][ind2];
-               }
             }
         }
-    return dp[m][n];
+        return dp[m][n];
     }
 }
