951503be960039772319f37e1a0bfccba67f4d71
POOJA GAUTAM <58748949+Pooja515@users.noreply.github.com>
2026-09-11 05:59:12 +0530

    Time: 0 ms (100%), Space: 47.5 MB (32.57%) - LeetHub

--- Java/Easy/0268-missing-number/0268-missing-number.java
+++ Java/Easy/0268-missing-number/0268-missing-number.java
@@ -1,6 +1,7 @@
 class Solution {
     public int missingNumber(int[] nums) {
-        int n = nums.length , xor =n;
+        int n = nums.length;
+        int xor = n;
         for(int i=0;i<n;i++){
             xor ^= nums[i] ^ i;
         }
