6a5fa424475b7c752557b6421f93e41584ba42e2
POOJA GAUTAM <58748949+Pooja515@users.noreply.github.com>
2026-09-14 01:54:04 +0530

    Time: 3 ms (24.88%), Space: 46.7 MB (21.67%) - LeetHub

--- Java/Medium/0695-max-area-of-island/0695-max-area-of-island.java
+++ Java/Medium/0695-max-area-of-island/0695-max-area-of-island.java
@@ -1,41 +1,41 @@
 class Solution {
-    int[][] dirs = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+    int[][] dirc = { { -1, 0 }, { 1, 0 }, { 0, -1 }, { 0, 1 } };
     public int maxAreaOfIsland(int[][] grid) {
-        if(grid == null || grid[0].length == 0) return 0;
-        int m = grid.length , n = grid[0].length;
+      
+        if (grid == null || grid.length == 0)
+            return 0;
+        int m = grid.length, n = grid[0].length;
+
         boolean[][] visited = new boolean[m][n];
-         int maxArea = 0;
-        for(int r = 0 ;r<m;r++){
-            for(int c=0;c<n;c++){
-                if(grid[r][c] == 1 && !visited[r][c] ){
-                    maxArea = Math.max(maxArea , bfs(r,c,grid,visited));
+        int maxArea= 0;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 1 && !visited[i][j]) {
+                   maxArea = Math.max(dfs(i, j, grid, visited, m, n), maxArea);
+                   
                 }
             }
         }
         return maxArea;
+
     }
-    int bfs(int r , int c , int[][] grid , boolean[][] visited){
-        int m = grid.length , n = grid[0].length;
-        int area = 1;
 
-        Queue<int[]> q = new ArrayDeque<>();
-        q.offer(new int[] {r,c});
+    int dfs(int r, int c, int[][] grid, boolean[][] visited, int m, int n) {
         visited[r][c] = true;
-
-        while(!q.isEmpty()){
-            int[] cur = q.poll();
-            for(int[] d : dirs){
-                int newr =cur[0] + d[0] ,newc = cur[1] + d[1];
-        
-                if(newr >= 0 && newr < m && newc >= 0 && newc < n && grid[newr][newc] == 1 && !visited[newr][newc] ){
-                 visited[newr][newc] = true;
-                 q.offer(new int[] {newr , newc});
-                 area++;
-                
-                }
+        int island =1;
+        for (int[] dir : dirc) {
+            int newr = r + dir[0], newc = c + dir[1];
+            if (newr >= 0 && newr < m && newc >= 0 && newc < n && grid[newr][newc] == 1 && !visited[newr][newc]) {
+                island += dfs(newr, newc, grid, visited, m, n);
             }
-           
-        }
-        return area;
+        } 
+        return island; 
     }
 }
+
+
+
+
+   
+    
