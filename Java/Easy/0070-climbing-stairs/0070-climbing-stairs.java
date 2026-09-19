4c1027d28e283409eae564a0a75c347aaa6366a7
POOJA GAUTAM <58748949+Pooja515@users.noreply.github.com>
2026-09-19 08:16:58 +0530

    Time: 0 ms (100%), Space: 42.1 MB (54.82%) - LeetHub

--- Java/Easy/0070-climbing-stairs/0070-climbing-stairs.java
+++ Java/Easy/0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,20 +1,17 @@
 class Solution {
-    int[] memo;
+
     public int climbStairs(int n) {
-        if(n <= 2) return n;
-        memo= new int[n+1];
+        if (n == 1 || n == 2)
+            return n;
+     
+        int p2 = 1 , p1 = 1;
 
-        Arrays.fill(memo,-1);
-
-        return f(n);
-    }
-    int f(int i){
-        if(i<=2) return i;
-
-        if(memo[i] != -1) return memo[i];
-
-        int climb1 = f(i-1);
-        int climb2 = f(i-2);
-        return memo[i] = climb1 + climb2;
+        for (int i = 2; i <= n; i++) {
+      
+            int cur = p2+p1;
+            p2 = p1;
+            p1 = cur;
+        }
+        return p1;
     }
 }
